3eefdb2075dec6abf890e1d15403b9ca1dfbc723
flash8627 <flash8627@hotmail.com>
2017-05-30 00:02:37 +0800

    cloud structure build project successfull

---
+++ icustom.cloud/src/main/java/com/gwtjs/icustom/cloud/ICustomCloudApplication.java
@@ -0,0 +1,12 @@
+package com.gwtjs.icustom.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ICustomCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICustomCloudApplication.class, args);
+	}
+}
