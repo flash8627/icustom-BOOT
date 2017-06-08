e153366883e072aafb5e7d65a812eb0d510d40b2
flash8627 <flash8627@hotmail.com>
2017-06-08 23:12:21 +0800

    bbs start

---
+++ icustom.bbs/src/main/java/com/gwtjs/icustom/bbs/ICustomBBSApplication.java
@@ -0,0 +1,26 @@
+/*
+ */
+
+package com.gwtjs.icustom.bbs;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.gwtjs.core.log.ICustomLogger;
+import com.gwtjs.core.log.ICustomLoggerFactory;
+
+@SpringBootApplication
+public class ICustomBBSApplication {
+	
+	private static final ICustomLogger log = ICustomLoggerFactory
+			.getLogger(ICustomBBSApplication.class);
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ICustomBBSApplication.class, args);
+		log.info("Running with Spring successfull------");
+	}
+	
+}
+
+
