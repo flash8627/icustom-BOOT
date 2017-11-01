0a8a451e451a4f01aba2274c6e5a32a11e74ee51
flash8627 <flash8627@hotmail.com>
2017-11-01 22:20:39 +0800

    add html area test and kind editor

--- icustom.common/icustom.common.htmlarea/src/test/java/com/gwtjs/icustom/common/htmlarea/dao/IHtmlAreaDaoTests.java
+++ icustom.common/icustom.common.htmlarea/src/test/java/com/gwtjs/icustom/common/htmlarea/dao/IHtmlAreaDaoTests.java
@@ -13,7 +13,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.gwtjs.icustom.common.htmlarea.vo.HtmlAreaVO;
-import com.gwtjs.icustom.entity.ResultWrapper;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
