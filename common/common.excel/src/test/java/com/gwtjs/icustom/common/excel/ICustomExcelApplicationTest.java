55f92e899b1889aa20c48fbb5fd2db7de1d85921
flash8627 <flash8627@hotmail.com>
2017-12-07 21:27:18 +0800

    text area update sql and icustom excel test case

--- common/common.excel/src/test/java/com/gwtjs/icustom/common/excel/ICustomExcelApplicationTest.java
+++ common/common.excel/src/test/java/com/gwtjs/icustom/common/excel/ICustomExcelApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ICustomExcelApplicationTest.class)
+@SpringBootTest(classes = ICustomExcelApplication.class)
 @WebAppConfiguration
 public class ICustomExcelApplicationTest {
 
--- common/common.excel/src/test/java/com/gwtjs/icustom/common/excel/ICustomExcelImportTest.java
+++ common/common.excel/src/test/java/com/gwtjs/icustom/common/excel/ICustomExcelImportTest.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = ICustomExcelImportTest.class)
+@SpringBootTest(classes = ICustomExcelApplication.class)
 @WebAppConfiguration
 public class ICustomExcelImportTest {
 
