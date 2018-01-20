b3d4a3a6eda50b12a4a8e6353e67d4afaf3b71cc
flash8627 <flash8627@hotmail.com>
2018-01-20 14:18:12 +0800

    ribbon eureka

--- common/icustom.htmlarea.api.test/src/test/java/com/gwtjs/icustom/common/htmlarea/api/HtmlAreaApiHelloTest.java
+++ common/icustom.htmlarea.api.test/src/test/java/com/gwtjs/icustom/common/htmlarea/api/HtmlAreaApiHelloTest.java
@@ -1,7 +1,7 @@
 package com.gwtjs.icustom.common.htmlarea.api;
 
 import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -15,39 +15,95 @@ public class HtmlAreaApiHelloTest {
 		// 指定 URL 和端口号
 		RestAssured.baseURI = "http://localhost:8086/";
 		RestAssured.port = 8086;
+		RestAssured.basePath = "/services/htmlarea";
+
+	}
+
+	/**
+	 * /{pageSize}/{curPage}
+	 */
+	@Test
+	public void testFindHtmlAreaPagePathParam() {
+		given().contentType("application/json").when()
+				.get("/htmlAreaService/findHtmlAreaPage/{pageSize}/{curPage}", 20, 1).then().statusCode(200);
+		given().get("/htmlAreaService/findHtmlAreaPage/20/1").then().body("pageVO.pageSize", equalTo(20));
+
+	}
+
+	/**
+	 * queryParam
+	 */
+	@Test
+	public void testFindHtmlAreaPageLike() {
+		given().queryParam("title", "A11111111A").queryParam("name", "1AAAA211111A").when()
+				.get("/htmlAreaService/findHtmlAreaPage/{pageSize}/{curPage}", 20, 1).then()
+				.contentType("application/json").statusCode(200);
+		given().get("/htmlAreaService/findHtmlAreaPage/20/1").then().body("pageVO.pageSize", equalTo(20));
+
 	}
 
 	@Test
 	public void testFindHtmlAreaPage() {
-		given().contentType("application/json").when().get("services/htmlarea/htmlAreaService/findHtmlAreaPage/20/1").then().statusCode(200);
-		given().param("pageSize", "8").when().get("services/htmlarea/htmlAreaService/findHtmlAreaPage/20/1").then().body("pageVO.pageSize", equalTo(20));
-		/*given().auth().basic("admin", "dddddd").when().*/get("services/htmlarea/htmlAreaService/findHtmlAreaPage/20/1").then()/*.body("pageVO",is(12))*/;
-		 //调用数组方法
-		 /*get("/1220562").then()
-		 //取顶级属性“title”
-		 .body("title", equalTo("i-custom"))
-		 //取下一层属性
-		 .body("rating.max", equalTo(10)).body("tags.size()", is(8))
-		 //取数组第一个对象的“name”属性
-		 .body("tags[0].name", equalTo("i-custom"))
-		 //判断数组元素
-		 .body("author", hasItems("[日] i-custom"))*/;
-		
+		given().headers("Accept", "application/json", "Content-Type", "application/json").when()
+				.get("/htmlAreaService/findHtmlAreaPage/20/1").then().statusCode(200);
+		given().headers("Accept", "application/json", "Content-Type", "application/json").param("pageSize", "8").when()
+				.get("/htmlAreaService/findHtmlAreaPage/20/1").then().body("pageVO.pageSize", equalTo(20));
+		/* given().auth().basic("admin", "dddddd").when(). */
+		get("/htmlAreaService/findHtmlAreaPage/20/1").then().body("pageVO.pageSize", is(20));
 	}
 
 	@Test
-	public void testSearchBook() {
-		/*
-		 * given().param("q", "java8").when().get("/search").then().body("count", equalTo(2));
-		 * given().cookie("name", "xx").when().get("/xxx").then().body();
-		given().header("name", "xx").when().get("/xxx").then().body();
-		given().contentType("application/json").when().get("/xxx").then().body();*/
-		/*given().auth().basic(username, password).when().get("/secured").then().statusCode(200);
-		given().auth().oauth(..);
-		given().auth().oauth2(..);*/
+	public void testSaveOrUpdateHtmlArea() {
+		/*given().parameters("name", "中文试一下", "title", "中文试一下", "mappingUrl", "fasdfasdf",
+				"content", "asdfasdf中文试一下", "description", "中文试一下", "validFromDate", "2017-12-04").expect().statusCode(500).when()
+				.post("/saveOrUpdate");*/
+		final String bodyString = "{\"name\":\"中文试一下\",\"title\":\"中文试一下\",\"mappingUrl\":\"fasdfasdf\",\"content\":\"asdfasdf中文试一下\",\"description\":\"中文试一下\",\"programFilterEnable\":true,\"category\":\"\",\"validFromDate\":\"2017-12-04\",\"validToDate\":\"2017-12-04\",\"htmlAreaId\":\"0\"}";
+		   
+	    given().
+	    contentType("application/json").
+	    request().body(bodyString).
+	    expect().
+	      statusCode(200).
+	      body(
+	    		  "status", is(1),/*
+	      "order.deleteDate", is(nullValue()),*/
+	      "success", equalTo(true)).
+	    when().
+	    post("/saveOrUpdate");
 
 	}
-	
-	
+
+	/**
+	 * given().headers("Accept", "application/json", "Content-Type",
+	 * "application/json")
+	 */
+	@Test
+	public void testSearchContentType() {
+		// 验证 index 的值不大于 20
+		given().headers("Accept", "application/json", "Content-Type", "application/json")
+				.get("/htmlAreaService/findHtmlAreaPage/20/1").then().assertThat()
+				.body("pageVO.curPage", lessThanOrEqualTo(20));
+
+		// 数组的 size 为12
+		given().contentType("application/json").get("/htmlAreaService/findHtmlAreaPage/20/1").then().assertThat()
+				.body("result", hasSize(12));
+
+		// 验证 result 中每个元素的 htmlAreaId 属性值都大于 0
+		given().contentType("application/json").get("/htmlAreaService/findHtmlAreaPage/20/1").then().assertThat()
+				.body("result.htmlAreaId", everyItem(greaterThan(0)));
+
+		/*
+		 * given().param("q", "java8").when().get("/search").then().body("count",
+		 * equalTo(2)); given().cookie("name", "xx").when().get("/xxx").then().body();
+		 * given().header("name", "xx").when().get("/xxx").then().body();
+		 * given().contentType("application/json").when().get("/xxx").then().body();
+		 */
+		/*
+		 * given().auth().basic(username,
+		 * password).when().get("/secured").then().statusCode(200);
+		 * given().auth().oauth(..); given().auth().oauth2(..);
+		 */
+
+	}
 
 }
